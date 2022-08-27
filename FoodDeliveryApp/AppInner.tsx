98830951040c4deac7e8a0f74f9d940e143ab426
scw3812 <scw3812@naver.com>
2022-08-27 13:02:48 +0900

    auto login

--- FoodDeliveryApp/AppInner.tsx
+++ FoodDeliveryApp/AppInner.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect, useCallback} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useSelector} from 'react-redux';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 import Orders from './src/pages/Orders';
 import Settings from './src/pages/Settings';
@@ -10,6 +11,8 @@ import Delivery from './src/pages/Delivery';
 import SignIn from './src/pages/SignIn';
 import SignUp from './src/pages/SignUp';
 import type {RootState} from './src/store/reducer';
+import userSlice from './src/slices/userSlice';
+import {useAppDispatch} from './src/store';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -28,6 +31,24 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+  const dispatch = useAppDispatch();
+
+  const autoLogin = useCallback(async () => {
+    const refreshToken = await EncryptedStorage.getItem('refreshToken');
+    if (refreshToken) {
+      dispatch(
+        userSlice.actions.setUser({
+          name: '',
+          email: '',
+          accessToken: '',
+        }),
+      );
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    autoLogin();
+  }, [autoLogin]);
 
   return (
     <NavigationContainer>
