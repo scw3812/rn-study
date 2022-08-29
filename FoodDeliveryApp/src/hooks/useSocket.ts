559fdade19f72c89f2a21b0c23bf9ea5822f986a
scw3812 <scw3812@naver.com>
2022-08-29 23:30:16 +0900

    socket 세팅

---
+++ FoodDeliveryApp/src/hooks/useSocket.ts
@@ -0,0 +1,22 @@
+import {useCallback} from 'react';
+import {io, type Socket} from 'socket.io-client';
+import Config from 'react-native-config';
+
+let socket: Socket | undefined;
+const useSocket = (): [Socket | undefined, () => void] => {
+  const disconnect = useCallback(() => {
+    if (socket) {
+      socket.disconnect();
+      socket = undefined;
+    }
+  }, []);
+  if (!socket) {
+    socket = io(Config.API_URL, {
+      transports: ['websocket'],
+      path: '/socket-io',
+    });
+  }
+  return [socket, disconnect];
+};
+
+export default useSocket;
