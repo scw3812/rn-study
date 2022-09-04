c7bcd2810f1252b33d99fd95dfcf52884e2b82a1
scw3812 <scw3812@naver.com>
2022-09-04 16:36:12 +0900

    주문 목록

--- FoodDeliveryApp/src/pages/Orders.tsx
+++ FoodDeliveryApp/src/pages/Orders.tsx
@@ -1,10 +1,23 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, FlatList} from 'react-native';
+import React, {useCallback} from 'react';
+import {useSelector} from 'react-redux';
+
+import {RootState} from '../store/reducer';
+import type {Order} from '../slices/orderSlice';
+import EachOrder from '../components/EachOrder';
 
 const Orders = () => {
+  const orders = useSelector((state: RootState) => state.order.orders);
+  const renderItem = useCallback(({item}: {item: Order}) => {
+    return <EachOrder item={item} />;
+  }, []);
   return (
     <View>
-      <Text>Orders</Text>
+      <FlatList
+        data={orders}
+        keyExtractor={item => item.orderId}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
--- FoodDeliveryApp/src/store/reducer.ts
+++ FoodDeliveryApp/src/store/reducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from '@reduxjs/toolkit';
+import orderSlice from '../slices/orderSlice';
 
 import userSlice from '../slices/userSlice';
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
+  order: orderSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
